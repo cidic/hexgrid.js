a9d8bc36638f91226e865ce0a2c01640ae94deac
Carl Olsen <cidic.conspiracy@gmail.com>
2011-07-31 23:35:49 +0000

    pathfinding stable

--- js/path_finding.js
+++ js/path_finding.js
@@ -1,11 +1,23 @@
-function hex_distance(x1,y1,x2,y2) {
-	dx = Math.abs(x2-x1);
-	dy = Math.abs(y2-y1);
+function node_distance(node1,node2){
+    var x1 = node1.center.x,
+        y1 = node1.center.y,
+        x2 = node2.center.x,
+        y2 = node2.center.y,
+        dx = Math.abs(x2-x1),
+        dy = Math.abs(y2-y1);
+        
 	return Math.round(Math.sqrt((dx*dx) + (dy*dy)));
 }
-
+var currentPathID = 0;
 function node_distance(node1,node2){
-    return hex_distance(node1.x,node1.y,node2.x,node2.y);   
+    var x1 = node1.center.x,
+        y1 = node1.center.y,
+        x2 = node2.center.x,
+        y2 = node2.center.y,
+        dx = Math.abs(x2-x1),
+        dy = Math.abs(y2-y1);
+        
+    return Math.round(Math.sqrt((dx*dx) + (dy*dy)));
 }
 var currentPathID = 0;
 function path(start_x, start_y, end_x, end_y) {
@@ -32,39 +44,59 @@ function path(start_x, start_y, end_x, end_y) {
     startNode.f = 0;
     startNode.g = 0;
     
-    var currentNode = startNode;;
+    var currentNode = startNode;
     
-    while(currentNode.x != endNode.x && currentNode.y != endNode.y){
+    while(currentNode != endNode){
         
-        // set pathID
-        currentNode.pathID = currentPathID;
-        // calculate values
-        // currentNode.g should already be set
-        currentNode.h = node_distance(currentNode, endNode);
-        currentNode.f = currentNode.g + currentNode.h;
         
-        var adj = currentNode.get_adjacent.filter(function(node) { return node.impassible !== false; });    
+        currentNode.pathID = currentPathID; // Mark this node as being in the closed set.
+        
+        var adj = currentNode.get_adjacent.filter(function(node) {
+             // Retrieve the neighbours of this node which are passable not in the closed set.
+            return !node.impassible && (node.pathID != currentPathID);
+        });    
         
         _.each(adj, function(adjNode){
-            if(adjNode.pathID != currentPathID){
-                var d = node_distance(currentNode,adjNode);
-                
-                adjNode.g = currentNode.g + d;
-                
-                priorityQueue.push(adjNode);
+            var d = node_distance(currentNode,adjNode);
+            console.log(d);
+            var adjNode_g = currentNode.g + d;
+            
+            if (adjNode.pathID == -currentPathID) {
+                // adjNode exists in the open set.
+                if (adjNode.g < adjNode_g) return; // The new path is not better.
+                priorityQueue.remove(adjNode);
             }
+            else adjNode.pathID = -currentPathID; // Mark as being in the open set.
+            
+            adjNode.g = adjNode_g;
+            var adjNode_h = node_distance(adjNode, endNode)
+            adjNode.f = adjNode_g + adjNode_h;
+            adjNode.parent = currentNode;
+            priorityQueue.push(adjNode);
+        
         });
 
         // get last node from priority queue
         currentNode = priorityQueue.pop();
+        
+        /*
+        if(priorityQueue.length === 0){
+            console.log('priorityQueue.length === 0');
+            return false;
+        }
 
+        */
         if(typeof currentNode == 'undefined'){
+            console.log('typeof currentNode == undefined');
             return false;   
         }
     }
     console.log(priorityQueue);
-    
-    _.each(priorityQueue.content, function(hex) {
+        // The actual path must be constructed using the parent pointers.
+        var path = [currentNode];
+        while (currentNode = currentNode.parent) path.unshift(currentNode);        
+        
+    _.each(path, function(hex) {
        hex.setColor('yellow'); 
     });
     
